5ac16a0a345b998b068af9439bd198ff407be10e
Thomas Mason <thomasm789@users.noreply.github.com>
2020-11-16 21:47:27 +0000

    Update package.js

--- package.js
+++ package.js
@@ -29,7 +29,7 @@
         function AllSourceProvider() {
             this.sourceProviderMetadata = {
                 name: 'Watch now',
-                premium: true
+                premium: false
             };
         }
         AllSourceProvider.prototype.search = function (env, request) {
